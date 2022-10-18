17f7174e3c723e2987b4db7bc983580309187b21
Owen Onions <owenonions@gmail.com>
2022-10-18 22:26:48 +0200

    Ini commit with 12-factor app principles

---
+++ octopus/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from octopus.users.models import User
+from octopus.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
